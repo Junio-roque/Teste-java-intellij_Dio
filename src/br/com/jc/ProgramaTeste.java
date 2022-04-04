c721148c2462e3c7d4223bcf3be4babfc139458b
JC <cezar_junio@yahoo.com.br>
2022-04-04 08:23:51 -0300

    first commit

---
+++ src/br/com/jc/ProgramaTeste.java
@@ -0,0 +1,17 @@
+package br.com.jc;
+
+import br.com.jc.model.Gato;
+
+public class ProgramaTeste {
+    public static void main(String[] args) {
+
+        Gato gato = new Gato();
+        System.out.println("Gato");
+
+
+
+       /* int a = 5;
+        int b = 7;
+        System.out.println("Hello world!" +(a+b));*/
+    }
+}
